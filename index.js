d323ea2f5859f69e1f8229c44bc88ff7a2844065
Chris Cardle <cdcardle@gmail.com>
2018-10-01 16:02:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-var customerName = 'bob'
-const leastFavoriteCustomer = 'sour fred'
+var customerName = "bob";
+const leastFavoriteCustomer = 'joe';
 
 function upperCaseCustomerName() {
-  return customerName = customerName.toUpperCase()
+  customerName = customerName.toUpperCase();
 }
 
-function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer = 'sour fred'
+function setBestCustomer() {
+  bestCustomer = 'not bob';
 }
 
-
-function setBestCustomer(){
-  bestCustomer = 'not bob'
+function overWriteBestCustomer(newFavorite) {
+  bestCustomer = newFavorite;
 }
 
-function overWriteBestCustomer(newFavorite){
-  bestCustomer = newFavorite
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'tom';
 }
 
-function congratulateCustomer(){
-  return `congrats ${favoriteCustomer}`
-}
-
-function attemptTwoFavoriteCustomers(){
-    let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
+function attemptTwoFavoriteCustomers() {
+  // let favoriteCustomer = 'tom';
+  // let favoriteCustomer = 'joe';
+  throw new SyntaxError('Cannot redeclare variables')
 }
