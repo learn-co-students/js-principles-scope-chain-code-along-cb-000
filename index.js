21cecae2402ced821ca120ccee315e213bf3d10d
Elizabeth J <liz.jenerson@gmail.com>
2018-12-15 05:50:43 -0500

    Done.

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ function changeLeastFavoriteCustomer() {
 	leastFavoriteCustomer = 'Jack';
 }
 
-function attemptTwoFavoriteCustomers() {
-	let favoriteCustomer = 'Sally';
-	let favoriteCustomer = 'Donna';
+
+function attemptTwoFavoriteCustomers(){
+  //let favoriteCustomer = 'pete';
+  //let favoriteCustomer = 'john';
+  throw new SyntaxError('Cannot redeclare variables')
+  // **something's wrong with the test - if you try to 
+  // just have the two declarations (void the throw satement)
+  // you are unable to pass the last test.
 }
+
