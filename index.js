0e38a4d5c3da5eaed3e2c182c7f8a7203e613efc
Sergio Marrocoli <s.marrocoli@gmail.com>
2019-02-12 23:27:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 /* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+  return bestCustomer
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "gary neville";
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "sergio";
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = "JIM";
+  let favoriteCustomer = "TREV";
+}
